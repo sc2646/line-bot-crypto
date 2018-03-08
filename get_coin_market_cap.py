207d72909c02f3dff7eaa931614a8afa77e1ffa7
nataliechen <nataliechen@coupang.com>
2018-03-08 00:45:37 -0800

    create line bot to push text messages

--- get_coin_market_cap.py
+++ get_coin_market_cap.py
@@ -1,5 +1,4 @@
 import requests
-# import urllib2
 from bs4 import BeautifulSoup
 from linebot import LineBotApi, WebhookHandler
 from linebot.models import TextSendMessage
@@ -7,18 +6,28 @@ from linebot.models import TextSendMessage
 LINK = "https://coinmarketcap.com/"
 PRICE = "price"
 PERCENTAGE = "percentage"
-ACCESS_TOKEN = "6lWwrvOTgNh+eg1WXm9XUv47Yr3sjFKI/T1cJTSJyLGRAT2wvitx8dp3EKDiY4cukWr52C99J4oFYGpd2H/7KzAfRZYlou9E4XsZ1pK71t156zg79eOGRxoN+dNPRaDja6CKMH4+kzJJslm3gU9UywdB04t89/1O/w1cDnyilFU="
-USER_ID = "Uce312f440fb25d82bf07f7c943089969"
-CHANNEL_SECRET = "aae2b4b36e80ec3ddce3b42352492b6c"
+ACCESS_TOKEN = "YOUR_ACCESS_TOKEN"
+USER_ID = "YOUR USER ID"
+CHANNEL_SECRET = "YOUR CHANNEL SECRET"
+
+
+BTC = "BTC"
+ETH = "ETH"
+LTC = "LTC"
+NEO = "NEO"
+XMR = "XMR"
+
+BTC_TARGET_PRICE = 8500
+ETH_TARGET_PRICE = 800
+LTC_TARGET_PRICE = 210
+NEO_TARGET_PRICE = 130
+XMR_TARGET_PRICE = 350
 
 class CoinMarketCapScrapper:
     def __init__(self):
-        # self.driver = webdriver.PhantomJS()
-        # self.response =
         self.result = dict()
         self.line_bot_api = LineBotApi(ACCESS_TOKEN)
         self.handler = WebhookHandler(CHANNEL_SECRET)
-
 
     def scrape_coin_market_cap(self, url):
         # self.driver.get(url)
@@ -31,8 +40,6 @@ class CoinMarketCapScrapper:
             try:
                 currency =  row.find("td", class_="no-wrap currency-name").span.a.get_text()
                 price = row.find("td", class_="no-wrap text-right").a.get_text()
-                # percentage_change = row.find("td", class_="no-wrap percent-change text-right negative_change").get_text()
-                # print percentage_change
                 is_target = self.isBTC(currency) or self.isETH(currency) or self.isLTC(currency) or self.isNEO(currency) or self.isXMR(currency)
 
                 if(is_target):
@@ -42,36 +49,42 @@ class CoinMarketCapScrapper:
             except:
                 pass
 
-    def send_text(self):
-        if len(self.result) != 0 :
+    def send_text(self, content):
             try:
-                if self.result["BTC"][PRICE] > 8500:
-                    print("=============")
-                    content = "BTC price is now " + str(self.result["BTC"][PRICE] + "!")
-                    print(content)
-                    self.line_bot_api.push_messsage(USER_ID, TextSendMessage(text="HELLO!"))
+                self.line_bot_api.push_message(USER_ID, TextSendMessage(text=content))
+            except:
+                pass
+
+    def price_above_target(self):
+        if len(self.result) != 0:
+            try:
+                currencies = self.result.keys()
+                for currency in currencies:
+                    if self.result[currency][PRICE] < currency + "_TARGET_PRICE":
+                        content = currency + " price is now " + str(self.result[currency][PRICE] + "!")
+                        self.send_text(content)
             except:
                 pass
 
     def isBTC(self, name):
-        return name == "BTC"
+        return name == BTC
 
     def isETH(self, name):
-        return name == "ETH"
+        return name == ETH
 
     def isNEO(self, name):
-        return name == "NEO"
+        return name == NEO
 
     def isLTC(self, name):
-        return name == "LTC"
+        return name == LTC
 
     def isXMR(self, name):
-        return name == "XMR"
-
+        return name == XMR
 
     def process(self):
         self.scrape_coin_market_cap(LINK)
-        self.send_text()
+        self.price_above_target()
+
 
 if __name__ == '__main__':
     coin_market_cap_scraper = CoinMarketCapScrapper()
